301f03b16eaf55d8b3b97f99bf8092d4679367ee
Valepa2911 <megp17102017@mail.ru>
2022-12-10 23:03:04 +0400

    сохран.

--- Zadacha34/Program.cs
+++ Zadacha34/Program.cs
@@ -2,30 +2,37 @@
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-int[] mass = new int[20];
-Random random = new Random();
-res = 0;
+Console.WriteLine("Введите длину массива:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[n];
+zapolnenie(mass);
+Console.WriteLine("Массив: ");
+PrintArray(mass);
+
+void zapolnenie(int[] mass)
+{
+    for(int i = 0; i < n; i++)
+    {
+        mass[i] = new Random().Next(100,1000);
+    }
+}
+
+
+int count = 0;
 
 for (int i = 0; i < mass.Length; i++)
 {
-    if (i % 2 == 0)
-            res = res + mass[i];
-       
-Console.Write("Четные" + count);
+if (mass[i] % 2 == 0)
+count++;
 }
-void Zapolnenie(int[] mass)
-{
-    for (int i = 0; i < mass.Length; i++)
-{
-    mass[i] = random.Next(100, 1000);
-}
-  
-     Console.Write(" " + mass);
-}
+Console.WriteLine("четных" + " " + count);
+
+
 void PrintArray(int[] mass)
 {
-    for (int i = 0; i < mass.Length; i++)
+    for(int i = 0; i < mass.Length; i++)
     {
-        Console.Write(mass[i]+", ");
+        Console.Write(mass[i] + " ");
     }
+        Console.WriteLine();
 }
