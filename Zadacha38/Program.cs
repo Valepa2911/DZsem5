61c5d9d29126d28dfedf6076e37909de6cf46189
Valepa2911 <megp17102017@mail.ru>
2022-12-11 12:06:25 +0400

    сохран.

--- Zadacha38/Program.cs
+++ Zadacha38/Program.cs
@@ -1,3 +1,40 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 69.4 -22 0.4 78] ->56
 
+Console.WriteLine("Введите длину массива:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] mass = new double[n];
+for (int i = 0; i < mass.Length; i++)
+{
+    Console.Write($"Введите элемент массива, можно с запятой {i}: ");
+    mass[i] = Convert.ToDouble (Console.ReadLine());
+}
+double max = 0;
+double min = 0;
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i] > max)
+        {
+            max = mass[i];
+        }
+    if (mass[i] < min)
+        {
+            min = mass[i];
+        }
+}
+Console.WriteLine($"Максимальное = {max}, минимальное = {min}");
+
+double Raznica (double[] mass)
+{
+    double raz = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {if (min >= 0)
+    raz = max - min;
+    }
+    
+    {if (min < 0)
+    raz = max + min;    
+    }
+    return raz;
+}
+Console.WriteLine($"Разница: {Raznica(mass)}");
